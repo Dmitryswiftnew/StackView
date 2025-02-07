07688d82b1ed617d39e6c84b0adbfa2ac6d342fe
Dmitryswiftnew <vasilevswiftnew@gmail.com>
2025-02-07 16:53:36 +0300

    Initial Commit

---
+++ StackView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackView
+//
+//  Created by Dmitry on 7.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
